a92194d0e6ac2c36ba43945a3f855e9d47b4cd58
rekirt@163.com <rekirt@163.com>
2016-07-13 22:01:41 +0800

    recyclerview几种布局方式

--- app/src/main/java/com/example/lc/materialdemo/recyclerview/RecyclerViewActivity1.java
+++ app/src/main/java/com/example/lc/materialdemo/recyclerview/RecyclerViewActivity1.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -51,11 +52,18 @@ public class RecyclerViewActivity1 extends BaseActivity {
 
     private void initView() {
         mRecyclerView  = (RecyclerView) findViewById(R.id.rv_view);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        LinearLayoutManager 现行管理器，支持横向、纵向。
+//        GridLayoutManager 网格布局管理器
+//        StaggeredGridLayoutManager 瀑布就式布局管理器
+        //listview
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,true));
+        //gridviwe
+//        mRecyclerView.setLayoutManager(new GridLayoutManager(this,2));
+
         mRecyclerView.setAdapter(mAdapter = new HomeAdapter());
 
         //添加分割线
-        mRecyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL_LIST));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.HORIZONTAL_LIST));
     }
 
 
