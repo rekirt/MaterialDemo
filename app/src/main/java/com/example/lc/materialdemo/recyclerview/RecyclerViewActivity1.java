5802571bdf8b56f1e93ac844481f7ac1e8562480
rekirt@163.com <rekirt@163.com>
2016-07-17 11:35:42 +0800

    recycleview 基本使用，以及分割线，长按状态，单选，多选未实现

--- app/src/main/java/com/example/lc/materialdemo/recyclerview/RecyclerViewActivity1.java
+++ app/src/main/java/com/example/lc/materialdemo/recyclerview/RecyclerViewActivity1.java
@@ -5,9 +5,11 @@ import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -55,15 +57,35 @@ public class RecyclerViewActivity1 extends BaseActivity {
 //        LinearLayoutManager 现行管理器，支持横向、纵向。
 //        GridLayoutManager 网格布局管理器
 //        StaggeredGridLayoutManager 瀑布就式布局管理器
-        //listview
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,true));
-        //gridviwe
-//        mRecyclerView.setLayoutManager(new GridLayoutManager(this,2));
 
+        //createListView();
+
+        //createGridView();
+
+        createStaggeredGridLayout();
+
+
+        //添加移除动画
+        //https://github.com/gabrielemariotti/RecyclerViewItemAnimators
+    }
+
+    private void createStaggeredGridLayout() {
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(4,StaggeredGridLayoutManager.VERTICAL));
+        mRecyclerView.setAdapter(new StaggeredHomeAdapter(this,mDatas));
+    }
+
+    private void createGridView() {
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this,4));
+        mRecyclerView.addItemDecoration(new DividerGridItemDecoration(this));
         mRecyclerView.setAdapter(mAdapter = new HomeAdapter());
+    }
 
-        //添加分割线
-        mRecyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.HORIZONTAL_LIST));
+    private void createListView() {
+        //LinearLayoutManager.HORIZONTAL水平listview，VERTICAL垂直listview
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,true));
+        //添加分割线,分割线必须与listview的方向一致,VERTICAL的布局对应VERTICAL_LIST的分割线
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL_LIST));
+        mRecyclerView.setAdapter(mAdapter = new HomeAdapter());
     }
 
 
@@ -78,6 +100,22 @@ public class RecyclerViewActivity1 extends BaseActivity {
         @Override
         public void onBindViewHolder(MyViewHolder holder, int position){
             holder.tv.setText(mDatas.get(position));
+            holder.itemView.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                }
+            });
+
+            holder.itemView.setOnLongClickListener(new View.OnLongClickListener()
+            {
+                @Override
+                public boolean onLongClick(View v)
+                {
+                    return false;
+                }
+            });
         }
 
         @Override
@@ -93,5 +131,9 @@ public class RecyclerViewActivity1 extends BaseActivity {
             }
         }
     }
+
+
+
+
 
 }
